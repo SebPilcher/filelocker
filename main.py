7ebbb76a8104c574eeb05a0b77b08f4ea5678dbc
stpilcher <s.t.pilcher@student.saintkentigern.com>
2023-05-24 12:31:54 +1200

    Updated GUI and made some other changes

--- main.py
+++ main.py
@@ -1,81 +1,110 @@
 import time
+import os
 from hashlib import sha256
-import tkinter
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-import Crypto.Cipher
+from tkinter import filedialog
 from Crypto.Cipher import AES
 import base64
 
-def test():
-    goto = input("Would you like to encrypt or decrypt data? ")
-    if goto == "encrypt" or goto == "Encrypt":
-        encrypt()
-    elif goto == "decrypt" or goto == "Decrypt":
-        decrypt()
-    else:
-        print("Invalid Answer")
-        test()
+class FileLockerGui: #Created class FileLockerGui for the gui
+    def __init__(self, root):
+        self.root = root 
+        root.title("FileLocker") #Name the window to "FileLocker"
+
+        root.minsize(250, 0) #The minimum width is 400px
+        root.resizable(0,0) #You can not resize the window
+
+        self.logo = tk.Label(root)
+        self.logo.pack()
+
+
+        self.operation = tk.StringVar(value="Choose an Option")
+        self.button_frame = ttk.LabelFrame(root, text="Test")
+        self.button_frame.pack(pady=5)
+        self.encrypt_button = ttk.Radiobutton(self.button_frame, text = "Encrypt", variable = self.operation, value = "Encrypt")
+        self.encrypt_button.grid(column=1, row=1)
+        self.decrypt_button = ttk.Radiobutton(self.button_frame, text = "Decrypt", variable = self.operation, value = "Decrypt")
+        self.decrypt_button.grid(column=2, row=1)
     
-def start():
-    print("\n ███████╗██╗██╗     ███████╗██╗      █████╗  █████╗ ██╗  ██╗███████╗██████╗ \n ██╔════╝██║██║     ██╔════╝██║     ██╔══██╗██╔══██╗██║ ██╔╝██╔════╝██╔══██╗\n █████╗  ██║██║     █████╗  ██║     ██║  ██║██║  ╚═╝█████═╝ █████╗  ██████╔╝\n ██╔══╝  ██║██║     ██╔══╝  ██║     ██║  ██║██║  ██╗██╔═██╗ ██╔══╝  ██╔══██╗\n ██║     ██║███████╗███████╗███████╗╚█████╔╝╚█████╔╝██║ ╚██╗███████╗██║  ██║\n ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝ ╚════╝  ╚════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n")
-    time.sleep(1)
-#    load()
-    app()
+
+        self.password_frame = ttk.Frame(root)
+        self.password_frame.pack(pady=5, padx=10)
+
+        self.password_label = tk.Label(self.password_frame, text = "Enter Password")
+        self.password_label.grid(column=1, row=1)
+
+        self.password_entry = tk.Entry(self.password_frame, show = "*")
+        self.password_entry.grid(column=1,row=2)
+
+        self.show_password = tk.IntVar()
+        self.show_password_checkbox = ttk.Checkbutton(self.password_frame, text = "Show Password", variable=self.show_password, command = self.toggle_password_visibility)
+        self.show_password_checkbox.grid(column=2, row=2)
 
 
-def load():
-    Loading=True
-    load=0 
-    while Loading==True:
-        print("Loading... |", end='\r')
-        time.sleep(0.25)
-        print("Loading... /", end='\r')
-        time.sleep(0.25)
-        print("Loading... --", end='\r')
-        time.sleep(0.25)
-        print("Loading... \ ", end='\r')
-        time.sleep(0.25)
-        load=load+1
-        if load>=2:
-            Loading=False
-            print("Loading Complete. Launching App!")
-            time.sleep(1)
+
+        self.file_select = ttk.Button(root, text = "Select File", command = self.select_file)
+        self.file_select.pack(pady=5)
+
+        self.process_button = ttk.Button(root, textvariable=self.operation)
+        self.process_button.pack()
+
+
+        self.filepath_display = tk.Label(root, text = "")
+        self.filepath_display.pack()
+
+
+
+
+    def toggle_password_visibility(self):
+        """Toggle password visibility based on checkbox state"""
+        if self.show_password.get() == 1:
+            self.password_entry.configure(show = "")
+        else:
+            self.password_entry.configure(show = "*")
+    
+    def select_file(self):
+
+        self.file_path = filedialog.askopenfilename() 
+        self.filepath_display.configure(text = self.file_path)
+    
+    def process(self):
+
+        password = self.password_entry.get()
+        if self.file_path and password:
+            operation = self.operation_var.get()
+            if operation == "Encrypt":
+                self.encrypt(self.file_path)
+                pass
+            elif operation == "Decrypt":
+                # Perform decryption on selected file
+                pass
+            # Display success message
+            tk.messagebox.showinfo("Success", f"File {operation}ed successfully!")
+        else:
+            # Display error message if file path or password is not provided
+            tk.messagebox.showerror("Error", "Please provide a file and password.")
+
+    def encrypt(self, filepath):
+        plaintext = bytes(input('Enter Data you want to encrypt:\n> '), "utf-8")
+
+        pw = input('Enter Password:\n> ')
+        pw = pw.encode("utf-8")
+        key = sha256(pw).digest()
+
+        cipher = AES.new(key, AES.MODE_ECB)
+        if len(plaintext) % AES.block_size == 0:
             return
-
-def app():
-    mainWindow=Tk()
-    mainWindow.geometry('500x300')
-    mainWindowFrame=tkinter.Frame(mainWindow)
-    mainWindowFrame.grid()
-
-    label = tkinter.Label(mainWindowFrame, text="test").grid(column=0, row=0)
-    button = tkinter.Button(mainWindowFrame, text="quit", command=mainWindow.quit).grid(column=0, row=1)
-    label2 = tkinter.Label(mainWindowFrame, text="test2").grid(column=1, row=2)
-    mainWindowFrame.grid_propagate(1)
-    
-
-    mainWindow.mainloop()
-
-def encrypt():
-    plaintext = bytes(input('Enter Data you want to encrypt:\n> '), "utf-8")
-
-    pw = input('Enter Password:\n> ')
-    pw = pw.encode("utf-8")
-    key = sha256(pw).digest()
-
-    cipher = AES.new(key, AES.MODE_ECB)
-    plaintext = plaintext + b'\0' * (AES.block_size - len(plaintext) % AES.block_size)
-    ciphertext = cipher.encrypt(plaintext)
-    base64_ciphertextbytes = base64.b64encode(ciphertext)
-    base64_ciphertext = base64_ciphertextbytes.decode("utf-8")
-    print(base64_ciphertext)
-    
-
-
-
+        else:
+            plaintext = plaintext + b'\0' * (AES.block_size - len(plaintext) % AES.block_size)
+        ciphertext = cipher.encrypt(plaintext)
+        base64_ciphertextbytes = base64.b64encode(ciphertext)
+        base64_ciphertext = base64_ciphertextbytes.decode("utf-8")
+        print(base64_ciphertext)
 
     
+
+
 
 def decrypt():
     base64_ciphertext = input('Enter Data you want to decrypt:\n> ')
@@ -89,12 +118,12 @@ def decrypt():
     
     plaintext = plaintext.rstrip(b'\0')
 
-    print(plaintext.decode("utf-8"))
+    print(plaintext.decode("utf-8"))   
 
     
 if __name__ == '__main__':
-    # start()    
-    test()
-
+    root = tk.Tk()
+    FileLockerGui(root)
+    root.mainloop()
 
 #take a password, encrypt it in sha256, then encrypt the file/files with aes256 using the encrypted password string. Give encrypted file .LKD extension. Done using hashlib and pyCryptoDome. Also, apparently it is good to encode the AES encryption with base64 if using 'text-only channels'
